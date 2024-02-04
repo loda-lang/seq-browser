bb1dd2bead10d745e4135d47f83ab801c12aa932
Christian Krause <me@ckrause.org>
2024-02-04 10:57:35 +0100

    improve start script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export PATH=$PWD/venv/bin:$PATH
-killall flask
+
+echo "Restarting seq-browser"
+killall flask 2> /dev/null
 nohup flask --app seq-browser run > seq-browser.out 2>&1 &
